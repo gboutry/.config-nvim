42afe96811f044ca4867496220a1b5f60cd580d6
gboutry <boutryguillaume1@gmail.com>
2022-12-19 09:19:21 +0100

    fix: remove custom warning symbols

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -16,23 +16,15 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
   ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
+  ['<C-Space>'] = cmp.mapping.complete(),
 })
-
 lsp.setup_nvim_cmp({
   mapping = cmp_mappings
 })
 
 lsp.set_preferences({
     suggest_lsp_servers = false,
-    sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
-    }
 })
-
 vim.diagnostic.config({
     virtual_text = true,
 })

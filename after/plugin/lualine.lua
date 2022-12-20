f9cc5a201c98fb9fd0c3cbcfef78b66899e9c4fb
gboutry <boutryguillaume1@gmail.com>
2022-12-20 11:10:59 +0100

    feat: add lualine

---
+++ after/plugin/lualine.lua
@@ -0,0 +1,8 @@
+local lualine = require("lualine")
+
+lualine.setup({
+    options = {
+        theme = 'onedark'
+    },
+    sections = { lualine_a = {'FugitiveHead'} }
+})
--- lua/gboutry/packer.lua
+++ lua/gboutry/packer.lua
@@ -56,4 +56,8 @@ return require('packer').startup(function(use)
         'm-demare/hlargs.nvim',
         requires = { 'nvim-treesitter/nvim-treesitter' }
     }
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
 end)

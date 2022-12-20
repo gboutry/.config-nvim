f67a62b7bf912d56fdaf7b58a53777582166b5eb
gboutry <boutryguillaume1@gmail.com>
2022-12-20 17:26:28 +0100

    fix: map split panels movements to arrows

--- lua/gboutry/remap.lua
+++ lua/gboutry/remap.lua
@@ -23,7 +23,7 @@ vim.keymap.set("n", "=", vim.lsp.buf.format)
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- move between split window
-vim.keymap.set("n", "<C-h>", [[:wincmd h<CR>]])
-vim.keymap.set("n", "<C-j>", [[:wincmd j<CR>]])
-vim.keymap.set("n", "<C-k>", [[:wincmd k<CR>]])
-vim.keymap.set("n", "<C-l>", [[:wincmd l<CR>]])
+vim.keymap.set("n", "<C-Left>", [[:wincmd h<CR>]])
+vim.keymap.set("n", "<C-Down>", [[:wincmd j<CR>]])
+vim.keymap.set("n", "<C-Up>", [[:wincmd k<CR>]])
+vim.keymap.set("n", "<C-Right>", [[:wincmd l<CR>]])

2a2701a7e075c33365912b92e0b940946a7fd4ea
gboutry <boutryguillaume1@gmail.com>
2022-12-20 11:22:06 +0100

    fix: add split pane missing bindings

--- lua/gboutry/remap.lua
+++ lua/gboutry/remap.lua
@@ -24,4 +24,6 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 
 -- move between split window
 vim.keymap.set("n", "<C-h>", [[:wincmd h<CR>]])
+vim.keymap.set("n", "<C-j>", [[:wincmd j<CR>]])
+vim.keymap.set("n", "<C-k>", [[:wincmd k<CR>]])
 vim.keymap.set("n", "<C-l>", [[:wincmd l<CR>]])
